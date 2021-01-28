efea79f493729b8f4d2f9bad8f2c78f03c876204
fredrik <fredrikbach1@gmail.com>
2021-01-28 14:55:25 +0100

    Adds maven package structure

---
+++ text-based-game/src/main/java/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+	public static void main(String[] args) {
+	System.out.println("Hello World!");
+	
+	
+	}
+	
+	public static String printHello() {
+		return "Hello world";
+	}
+}
