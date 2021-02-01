f97a12b06b21ffe03500e02a81a4d58ae336e6c9
fredrik <fredrikbach1@gmail.com>
2021-02-01 15:00:30 +0100

    corrects spelling errors

--- text-based-game/src/test/java/EnviromentTest.java
+++ text-based-game/src/test/java/EnviromentTest.java
@@ -1,11 +1,12 @@
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class EnviromentTest {
 
     @BeforeEach
     void setUp() {
-        Enviroment env = new Enviroment();
+        Enviroment env = new Enviroment(name, description, listOfActions, listOfInteractables);
     }
 
 
