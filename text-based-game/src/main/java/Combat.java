3e379569246ef2888c6293ca07069014a05ff67e
uzair-c <hej@uzair.se>
2021-02-01 15:45:36 +0100

    combat method

--- text-based-game/src/main/java/Combat.java
+++ text-based-game/src/main/java/Combat.java
@@ -1,8 +1,24 @@
 public class Combat {
 
+    public void combat (Player player, Player enemy) {
 
-    public void damage() {
+        do {
+            double chance = Math.random();
+            if (chance < 0.5) {
+                System.out.println(player.getName() + " damages " + enemy.getName());
+                enemy.takeDamage(10);
+            }else {
+                System.out.println(enemy.getName() + " damages " + player.getName());
+                player.takeDamage(10);
+            }
 
+        } while (player.getHealthPoints() > 0 && enemy.getHealthPoints() > 0);
+
+        if (player.getHealthPoints() > enemy.getHealthPoints()) {
+            System.out.println("**** " + player.getName() + " wins ****");
+        } else {
+            System.out.println("**** " + enemy.getName() + " wins ****");
+        }
     }
 
 
