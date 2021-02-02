ceee98a227e82eba38d00e73789574331fb2d65e
Simon Riemertzon <simon.riemertzon@gmail.com>
2021-02-02 13:14:23 +0100

    Add functionality for interactable objects in game

--- text-based-game/src/main/java/Door.java
+++ text-based-game/src/main/java/Door.java
@@ -1,10 +1,8 @@
 public class Door implements Interactable {
-
     private Boolean isOpen = false;
     private String name = "a door";
     private String description = "An really old door made out of murky dark wood. On the right side of the door, there is a handle in pure gold. " +
             "The sparkling handle seems out of place in comparisson to the rest of the door.";
-
 
     @Override
     public void interact(Player player) {
