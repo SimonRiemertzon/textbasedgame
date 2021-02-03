089e64a21031852e915ca63eadc31c9ebadb1198
uzair-c <hej@uzair.se>
2021-02-04 00:54:00 +0100

    java docs to Combat.java

--- text-based-game/src/main/java/Combat.java
+++ text-based-game/src/main/java/Combat.java
@@ -1,14 +1,19 @@
-
 import java.util.concurrent.ThreadLocalRandom;
 public class Combat {
-
+    /**
+     * This class gets players from Player class, generate a battle between player and enemy before printing the final winner.
+     * @param player from Player class.
+     * @param enemy from Player class.
+     */
     public static void combat (Player player, Player enemy) {
-
 
         do {
             double chance = Math.random();
             int rand = ThreadLocalRandom.current().nextInt(10)+1;
 
+            /**
+             * Random nr is generated to choose who's turn it is and thereby the damage to opponent.
+             */
             if (chance < 0.5) {
                 System.out.println(player.getName() + " damages " + enemy.getName());
                 enemy.takeDamage(rand);
@@ -22,7 +27,15 @@ public class Combat {
                 e.printStackTrace();
             }
 
+            /**
+             * do while loop will run until both hit 0 healthPoints.
+             */
+
         } while (player.getHealthPoints() > 0 && enemy.getHealthPoints() > 0);
+
+        /**
+         * Announcement of winner.
+         */
 
         if (player.getHealthPoints() > enemy.getHealthPoints()) {
             System.out.println("**** " + player.getName() + " wins ****");
